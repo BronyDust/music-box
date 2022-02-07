14258d71020f76594604894894db3ce9e0543337
RyanWright <SivanoFFDS@gmail.com>
2022-02-07 15:55:22 +0700

    Add canvas element dynamically

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
-function main() {
+import './index.css';
 
+function main() {
+  const canvasElement = document.createElement('canvas');
+  canvasElement.style.width = '100vw';
+  canvasElement.style.height = '100vh';
+
+  document.body.appendChild(canvasElement);
 }
 
 main();
